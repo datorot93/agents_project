66847426bfb47e81cf1626115f9cd2edc233af30
alejandrolopezgiraldo <alopezgiraldo7@gmail.com>
2024-08-26 21:13:38 -0500

    changes in router file

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,5 @@
 import { createBrowserRouter } from "react-router-dom"
 import App from "./App"
-import Dashboard from "./components/routes/Dashboard"
-import Contact from "./components/routes/Contact"
 import Agents from "./components/routes/Agents"
 
 export const router = createBrowserRouter([
