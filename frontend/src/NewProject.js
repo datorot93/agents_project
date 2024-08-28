c8b967701d86bd95a932038e793744d457eb26b0
alejandrolopezgiraldo <alopezgiraldo7@gmail.com>
2024-08-27 21:57:12 -0500

    submit agents button disabled when clicked now working. Render the agents in the frontend

--- frontend/src/NewProject.js
+++ frontend/src/NewProject.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import apiService from './services/apiService'
+import CandidatesItem from './components/candidates/CandidatesItem'
 
 const ProjectDescription = () => {
     const [projectTitle, setProjectTitle] = useState('')
@@ -9,22 +10,32 @@ const ProjectDescription = () => {
     const [projectRequirements, setProjectRequirements] = useState('')
     const [projectResponsabilities, setProjectResponsabilities] = useState('')
     const [projectPreferredQualifications, setProjectPreferredQualifications] = useState('')
+    const [candidates, setCandidates] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [candidateResults, setCandidatesResult] = useState(false)
     const navigate = useNavigate();
 
     const startAgent = async (data) => {
+        setIsLoading(true);
         try {
             const url = `api/search_candidates`;
             const response_agents = await apiService.postWithoutToken(url, data);
             console.log(response_agents);
-            return response_agents; // Optionally return the response
+            setCandidates(response_agents.result.candidates || [])
+            setCandidatesResult(true)
+            //navigate('/candidates', {state: {candidates: candidates}})
+            //return response_agents; // Optionally return the response
+
         } catch (error) {
             console.error("Error in startAgent function:", error);
             // You can handle the error here, e.g., show a user-friendly message
             // throw error; // Uncomment this if you want to propagate the error to the caller
+        } finally {
+            setIsLoading(false);
         }
     };
 
-    startAgent()
+
     const handleForm = (e) => {
         e.preventDefault();
         const data = {
@@ -36,63 +47,83 @@ const ProjectDescription = () => {
         }
         console.log(data); // Form data object can be used here (e.g., send it to an API)
         startAgent(data)
-        navigate('/candidates')
-
-
     }
     
   return (
-    <div className="p-5" onSubmit={handleForm}>
-        <form className="w-full max-w-[600px] mx-auto p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <h1 className="text-5xl font-extrabold dark:text-white mb-10">Task Description</h1>
-            <div className="mb-5">
-                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Title</label>
-                <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
-                focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectTitle(e.target.value)} value={projectTitle}
-                placeholder="Python and React JS Engineer"></input>
+    <>
+        {
+            !candidateResults ? (
+                <div className="p-5" onSubmit={handleForm}>
+                <form className="w-full max-w-[600px] mx-auto p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <h1 className="text-5xl font-extrabold dark:text-white mb-10">Task Description</h1>
+                    <div className="mb-5">
+                        <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Title</label>
+                        <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+                        focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectTitle(e.target.value)} value={projectTitle}
+                        placeholder="Python and React JS Engineer"></input>
+                    </div>
+                    <div className="mb-5">
+                        <label
+                            htmlFor="large-input"
+                            className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
+                        >
+                            Job Description
+                        </label>
+                        <textarea
+                            id="large-input"
+                            className="block w-full height-6rem p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-sm 
+                            focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                            dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="We are seeking a skilled Python and React JS engineer to join our team." onChange={(e) => setProjectDescription(e.target.value)} value={projectDescription}>
+                        </textarea>
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Requirements</label>
+                        <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+                        focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectRequirements(e.target.value)} value={projectRequirements}
+                        placeholder="Write clean, maintainable, and efficient code; Ensure application performance"></input>
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Responsabilities</label>
+                        <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+                        focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectResponsabilities(e.target.value)} value={projectResponsabilities}
+                        placeholder="Proven experience with Python and React; Understanding of OOP"></input>
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preferred Qualifications</label>
+                        <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+                        focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectPreferredQualifications(e.target.value)} value={projectPreferredQualifications}
+                        placeholder="Experience with cloud services (AWS, Google Cloud, or Azure); Fluent english"></input>
+                    </div>
+                    <button 
+                        type="submit" 
+                        className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
+                        font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
+                        dark:focus:ring-blue-800 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                        disabled={isLoading}
+                        >
+                        {isLoading ? 'Loading...' : 'Submit'}
+                    </button>
+                </form>
             </div>
-            <div className="mb-5">
-                <label
-                    htmlFor="large-input"
-                    className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
-                >
-                    Job Description
-                </label>
-                <textarea
-                    id="large-input"
-                    className="block w-full height-6rem p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-sm 
-                    focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                    dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="We are seeking a skilled Python and React JS engineer to join our team." onChange={(e) => setProjectDescription(e.target.value)} value={projectDescription}>
-                </textarea>
-            </div>
-            <div className="mb-5">
-                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Requirements</label>
-                <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
-                focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectRequirements(e.target.value)} value={projectRequirements}
-                placeholder="Write clean, maintainable, and efficient code; Ensure application performance"></input>
-            </div>
-            <div className="mb-5">
-                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Responsabilities</label>
-                <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
-                focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectResponsabilities(e.target.value)} value={projectResponsabilities}
-                placeholder="Proven experience with Python and React; Understanding of OOP"></input>
-            </div>
-            <div className="mb-5">
-                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preferred Qualifications</label>
-                <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
-                focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setProjectPreferredQualifications(e.target.value)} value={projectPreferredQualifications}
-                placeholder="Experience with cloud services (AWS, Google Cloud, or Azure); Fluent english"></input>
-            </div>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
-            font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
-            dark:focus:ring-blue-800">Submit</button>
-        </form>
-    </div>
+            ) : (
+                <div className='flex flex-col items-center'>
+                    <form>
+                        <div className="mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                        {candidates.map((candidate) => (
+                            <CandidatesItem key={candidate.name} candidate={candidate} />
+                        ))}
+                        </div>
+                    </form>
+                </div>
+            )
+        }
+    </>
+
   )
 }
 
--- frontend/src/components/candidates/CandidatesList.js
+++ frontend/src/components/candidates/CandidatesList.js
@@ -1,20 +1,22 @@
+import { useLocation } from 'react-router-dom';
 import CandidatesItem from './CandidatesItem'
 import { dataCandidates } from './dataCandidate'
 import React from 'react';
 
-const CandidatesList = () => {  
-  if (!dataCandidates || dataCandidates.length === 0 || !dataCandidates[0].candidates) {
+const CandidatesList = () => {
+  {/*if (!candidates || candidates.length === 0) { //|| !dataCandidates[0].candidates
     return <div>No hay candidatos disponibles</div>;
-  }
+  }*/}
+  console.log("Render candidates list");
 
-  const candidates = dataCandidates[0].candidates;
+  //const candidates = candidates[0].candidates;
 
   return (
     <div className='flex flex-col items-center'>
       <form>
         <div className="mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {candidates.map((candidate) => (
-            <CandidatesItem key={candidate.name} candidates={candidate} />
+            <CandidatesItem key={candidate.name} candidate={candidate} />
           ))}
         </div>
       </form>
